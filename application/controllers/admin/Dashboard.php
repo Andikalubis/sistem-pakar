45908edc61b4ba56060269b952b7e311f4bdb870
zcrime <86516209+Zcrime@users.noreply.github.com>
2023-07-14 04:27:01 +0700

    fixed on controller (dashboard, auth)

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -3,14 +3,23 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller
 {
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Admin_model');
+        $this->load->helper('auth_helper');
+        if (!is_logged_in() || !is_admin()) {
+            redirect('auth/login');
+        }
+    }
+
     public function index()
     {
-        $tmp = array(
-            'title' => 'Dashboard',
-        );
-
-        $tmp['contents'] = $this->load->view('admin/index', $tmp, TRUE);
-
-        $this->load->view('admin/layout/template', $tmp);
+        // Ambil data pengguna dengan level admin dari database
+        $data['user'] = $this->Admin_model->get_admin_users();
+        
+        // Tampilkan data di view dashboard_admin
+        $this->load->view('admin/layout/template', $data);
     }
+
 }
+
--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -1,36 +1,81 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
+    
+    class Auth extends CI_Controller {
+    
+        public function __construct() {
+            parent::__construct();
+            $this->load->model('Auth_model');
+            $this->load->library('session');
+        }
 
-class Auth extends CI_Controller
-{
+    public function index() {
+        // Tampilkan halaman login
+        $this->load->view('auth/login');
+    }
+    
+    public function login() {
+        // Proses login
+        $username = $this->input->post('username');
+        $password = md5($this->input->post('password'));
 
-    /**
-     * Index Page for this controller.
-     *
-     * Maps to the following URL
-     * 		http://example.com/index.php/welcome
-     *	- or -
-     * 		http://example.com/index.php/welcome/index
-     *	- or -
-     * Since this controller is set as the default controller in
-     * config/routes.php, it's displayed at http://example.com/
-     *
-     * So any other public methods not prefixed with an underscore will
-     * map to /index.php/welcome/<method_name>
-     * @see https://codeigniter.com/userguide3/general/urls.html
-     */
-    public function index()
-    {
-        $this->load->view('/auth/login');
+        $user = $this->Auth_model->get_user($username, $password);
+        
+        if ($user) {
+            // Set session
+            $user_data = array(
+                'user_id' => $user->id_user,
+                'username' => $user->username,
+                'level' => $user->level
+            );
+            $this->session->set_userdata($user_data);
+
+            // Redirect ke halaman sesuai level
+            if ($user->level == 'admin') {
+                redirect('admin/dashboard');
+            } else {
+                redirect('user/dashboard');
+            }
+        } else {
+            // Jika login gagal, kembalikan ke halaman login
+            redirect('auth/login');
+        }
     }
 
-    public function login()
-    {
-        $this->load->view('/auth/login');
+    public function register() {
+        // Proses registrasi
+        $data = array(
+            'username' => $this->input->post('username'),
+            'password' => md5($this->input->post('password')),
+            'nama' => $this->input->post('nama'),
+            'alamat' => $this->input->post('alamat'),
+            'jenis_kelamin' => $this->input->post('jenis_kelamin'),
+            'email' => $this->input->post('email'),
+            'tlp' => $this->input->post('tlp'),
+            'level' => 'user'
+        );
+
+        $this->Auth_model->register_user($data);
+        
+        // Set session
+        $user_data = array(
+            'user_id' => $this->db->insert_id(),
+            'username' => $data['username'],
+            'level' => $data['level']
+        );
+        $this->session->set_userdata($user_data);
+
+        // Redirect ke halaman dashboard pengguna
+        redirect('user/dashboard');
     }
 
-    public function register()
-    {
-        $this->load->view('/auth/register');
+    public function logout() {
+        // Hapus session dan redirect ke halaman login
+        $this->session->unset_userdata('user_id');
+        $this->session->unset_userdata('username');
+        $this->session->unset_userdata('level');
+        redirect('auth/login');
     }
-}
+
+}   
+    
