1bedf1bf15b80a299abb3f3befe04b3916f44f52
inamfalahuddin <inamfalahuddin06@gmail.com>
2023-07-13 17:03:16 +0700

    add login and register

---
+++ application/views/admin/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php $this->load->view('front/layout/header') ?>
+</head>
+
+<body class="page-index">
+    <!-- ======= Header ======= -->
+    <header id="header" class="header d-flex align-items-center fixed-top">
+        <?php $this->load->view('front/layout/navbar') ?>
+    </header>
+    <!-- End Header -->
+
+    <?= $contents ?>
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer" class="footer">
+        <?php $this->load->view('front/layout/footer') ?>
+    </footer>
+    <!-- End Footer -->
+    <!-- End Footer -->
+
+    <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- <div id="preloader"></div> -->
+
+    <!-- Vendor JS Files -->
+    <script src="<?= base_url() ?>assets/template/front/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url() ?>assets/template/front/assets/vendor/aos/aos.js"></script>
+    <script src="<?= base_url() ?>assets/template/front/assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="<?= base_url() ?>assets/template/front/assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="<?= base_url() ?>assets/template/front/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="<?= base_url() ?>assets/template/front/assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="<?= base_url() ?>assets/template/front/assets/js/main.js"></script>
+</body>
+
+</html>
---
+++ application/views/auth/register.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Log in (v2)</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?= base_url(); ?>assets\tempate\admin\plugins\fontawesome-free\css\all.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="<?= base_url(); ?>assets\tempate\admin\plugins\icheck-bootstrap\icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= base_url(); ?>assets\tempate\admin\dist\css\adminlte.min.css">
+</head>
+
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <!-- /.login-logo -->
+        <div class="card card-outline card-primary">
+            <div class="card-header text-center">
+                <a href="<?= base_url(); ?>" class="h1"><b>Sistem</b>Pakar</a>
+                <br />
+                <span>Sistem Pakar Deteksi Minat Dan Bakat</span>
+            </div>
+            <div class="card-body">
+                <p class="login-box-msg">Daftar terlebih dahulu untuk memulai sesi kamu</p>
+
+                <form action="../../index3.html" method="post">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Nama">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Username">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" placeholder="Password">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" placeholder="Konfirmasi Password">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Telepon">
+                    </div>
+                    <div class="input-group mb-3">
+                        <select class="custom-select">
+                            <option>Pilih Jenis Kelamin</option>
+                            <option>Laki-laki</option>
+                            <option>Password</option>
+                        </select>
+                    </div>
+                </form>
+
+                <div class="social-auth-links text-center mt-2 mb-3">
+                    <a href="#" class="btn btn-block btn-primary">
+                        Sign up
+                    </a>
+                </div>
+                <!-- /.social-auth-links -->
+
+                <p class="mb-1">
+                    <a href="<?= base_url() ?>auth/login">Saya sudah memiliki akun</a>
+                </p>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+    </div>
+    <!-- /.login-box -->
+
+    <!-- jQuery -->
+    <script src="<?= base_url(); ?>assets\tempate\admin\plugins\jquery\jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?= base_url(); ?>assets\tempate\admin\plugins\bootstrap\js\bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?= base_url(); ?>assets\tempate\admin\dist\js\adminlte.min.js"></script>
+</body>
+
+</html>
