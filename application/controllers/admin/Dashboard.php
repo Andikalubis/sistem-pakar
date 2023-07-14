22798c1ed25e2317fd7ea776b358cfbe7fea65ca
zcrime <86516209+Zcrime@users.noreply.github.com>
2023-07-14 19:47:00 +0700

    commit

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -3,26 +3,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->model('Admin_model');
-        $this->load->helper('auth_helper');
-        if (!is_logged_in() || !is_admin()) {
-            redirect('auth/login');
-        }
-    }
-
     public function index()
     {
-        // Ambil data pengguna dengan level admin dari database
-        $data['user'] = $this->Admin_model->get_admin_users();
         $tmp = array(
             'title' => 'dashboard',
         );
 
-        // Tampilkan data di view dashboard_admin
-        $tmp['contents'] = $this->load->view('admin/index', $tmp, TRUE);
-        $this->load->view('admin/layout/template', $data);
+        $tmp['contents'] = $this->load->view('admin/layout/template', $tmp, TRUE);
+        $this->load->view('admin/layout/template', $tmp);
     }
 }
