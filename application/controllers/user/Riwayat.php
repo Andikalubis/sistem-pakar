865313381c88ee99e8869a24ad5d4748e1c867f0
Zcrime <anandika217@gmail.com>
2023-07-24 17:14:49 +0700

    Add View User Side

---
+++ application/controllers/user/Riwayat.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Riwayat extends CI_Controller
+{
+    public function index()
+    {
+        $data = array(
+            'title' => 'riwayat',
+        );
+
+        // Load model 
+        $this->load->model('Admin_model');
+        $this->load->model('User_model');
+        
+        // Ambil data riwayat dari model
+        $data['hasil'] = $this->Admin_model->getRiwayat();
+
+        foreach ($data['hasil'] as $row) {
+            $row->nama = $this->User_model->get_user_by_id($row->id_user);
+        }
+        
+        $data['contents'] = $this->load->view('user/pages/riwayat', $data, TRUE);
+        $this->load->view('user/layout/template', $data);
+    }
+}
---
+++ application/views/user/pages/riwayat.php
@@ -0,0 +1,71 @@
+<h3 class="text-center my-4">Riwayat Deteksi</h3>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card card-default">
+            <div class="card-body p-4">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Responsive Hover Table</h3>
+
+                                <div class="card-tools">
+                                    <div class="input-group input-group-sm" style="width: 150px;">
+                                        <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+
+                                        <div class="input-group-append">
+                                            <button type="submit" class="btn btn-default">
+                                                <i class="fas fa-search"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-hover text-nowrap">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Nama</th>
+                                            <th>Usia</th>
+                                            <th>Tanggal</th>
+                                            <th>Hasil</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php
+                                        $no=1;
+                                        foreach($hasil as $data) : ?>
+                                            <tr>
+                                                <td><?php echo $no++ ?></td>
+                                                <td><?php echo $data->nama ?></td>
+                                                <td><?php echo $data->bobot ?></td>
+                                                <td><?php echo $data->tanggal ?></td>
+                                                <td><?php echo $data->usia ?></td>
+                                                <td><?php echo $data->hasil ?></td>
+                                                <td>
+                                                    <a href="<?php echo base_url('admin/riwayat/').$data->id_hasil ?>" class="btn btn-sm btn-success">
+                                                        <i class="fas fa-show"></i>
+                                                    </a>
+                                                    <a href="<?php echo base_url('admin/riwayat/').$data->id_hasil ?>" class="btn btn-sm btn-danger">
+                                                        <i class="fas fa-trash"></i>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach; ?>
+                                </tbody>     
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                </div>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+    </div>
+</div>
