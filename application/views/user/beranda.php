36e2540e23dcbb1d654480b444a0f6bae2927179
Zcrime <anandika217@gmail.com>
2023-07-25 23:24:56 +0700

    fixed (nama berdasarkan username)

--- application/views/user/beranda.php
+++ application/views/user/beranda.php
@@ -1,4 +1,8 @@
 <div class="d-flex flex-column justify-content-center align-items-center" style="height: 80vh;">
-    <h1 class="text-center">Selamat Datang User di Perbandingan Metode Certainty Factor Dan Naive Bayes Pada Identifikasi Minat Dan Bakat Berdasarkan Multiple INTELLIGENCES</h1>
-
+    <h1 class="text-center">Selamat Datang <?= isset($username) ? $username : 'Your Acount Name' ?>....!! di Perbandingan Metode Certainty Factor Dan Naive Bayes Pada Identifikasi Minat Dan Bakat Berdasarkan Multiple INTELLIGENCES</h1>
+    <div class="social-auth-links text-center mt-2 mb-3">
+        <a href="<?php echo base_url('user/deteksi') ?>" class="btn btn-primary btn-block">
+            Mulai Deteksi
+        </a>
+    </div>
 </div>
