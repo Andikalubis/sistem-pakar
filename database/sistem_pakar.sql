7f7e5d9d5289e76ac3bd1a12e86717fe61db376c
Zcrime <anandika217@gmail.com>
2023-07-28 15:19:52 +0700

    Update Database

--- database/sistem_pakar.sql
+++ database/sistem_pakar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Jul 2023 pada 14.32
+-- Waktu pembuatan: 28 Jul 2023 pada 10.05
 -- Versi server: 10.4.25-MariaDB
 -- Versi PHP: 8.1.10
 
@@ -30,17 +30,44 @@ SET time_zone = "+00:00";
 CREATE TABLE `gejala` (
   `id_gejala` int(11) NOT NULL,
   `kode_gejala` varchar(25) DEFAULT NULL,
-  `nama_gejala` varchar(50) DEFAULT NULL,
-  `bobot` double DEFAULT NULL
+  `nama_gejala` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `gejala`
 --
 
-INSERT INTO `gejala` (`id_gejala`, `kode_gejala`, `nama_gejala`, `bobot`) VALUES
-(1, 'G1', 'sample', 0.1),
-(2, 'G2', 'sample', 0.2);
+INSERT INTO `gejala` (`id_gejala`, `kode_gejala`, `nama_gejala`) VALUES
+(4, 'G1', 'Suka Bercerita'),
+(5, 'G2', 'Suka Membaca'),
+(6, 'G3', 'Suka Menulis'),
+(7, 'G4', 'Suka Memecahkan Teka Teki'),
+(8, 'G5', 'Gemar Berdebat'),
+(10, 'G6', ' Melakukan Permainan Logika Seperti Catur'),
+(11, 'G7', 'Suka Membaca Buku Tentang Ilmu Pengetahuan '),
+(12, 'G8', 'Suka Berhitung Tanpa Alat Bantu'),
+(13, 'G9', 'Melakukan Permainan Yang Melibatkan Gambar atau 3D'),
+(14, 'G10', 'Mudah Mengenali Pola dan Berimajinasi'),
+(15, 'G11', 'Suka Menggambar atau Coret -Coret'),
+(16, 'G12', 'Bisa Memainkan Salah Satu Alat Musik'),
+(17, 'G14', 'Suka Bernyanyi'),
+(20, 'G14', 'Suka Mendengarkan Musik'),
+(21, 'G15', 'Suka Belajar Di Iringi Musik'),
+(22, 'G16', 'Suka Melakukan Kegiatan Fisik Seperti Bersepeda'),
+(23, 'G17', 'Suka Menari'),
+(24, 'G18', 'Suka Olahraga atau Bela Diri'),
+(25, 'G19', 'Suka Menirukan Gerak Tubuh Untuk Berbicara'),
+(26, 'G20', 'Senang Bekerja Sama Dengan Orang Lain, Kelompok'),
+(27, 'G21', 'Suka Berkenalan Dengan Orang Baru'),
+(28, 'G22', 'Mudah Bergaul atau Bersosialisasi'),
+(29, 'G23', 'Memiliki Empati Yang Tinggi Dengan Teman'),
+(30, 'G24', 'Suka Belajar Sendiri Maupun Bermain'),
+(31, 'G25', 'Suka Keadaan Tenang'),
+(32, 'G26', 'Menyukai Hobi Bersifat Pribadi'),
+(33, 'G27', 'Suka Dengan Binatang'),
+(34, 'G28', 'Senang Berjalan - Jalan Di Taman'),
+(35, 'G29', 'Senang Berkebun'),
+(36, 'G30', 'Suka Berkemah');
 
 -- --------------------------------------------------------
 
@@ -50,7 +77,7 @@ INSERT INTO `gejala` (`id_gejala`, `kode_gejala`, `nama_gejala`, `bobot`) VALUES
 
 CREATE TABLE `hasil` (
   `id_hasil` int(11) NOT NULL,
-  `id_user` int(11) DEFAULT NULL,
+  `nama` varchar(50) DEFAULT NULL,
   `bobot` double DEFAULT NULL,
   `tanggal` date DEFAULT NULL,
   `usia` int(50) DEFAULT NULL,
@@ -75,8 +102,14 @@ CREATE TABLE `kriteria` (
 --
 
 INSERT INTO `kriteria` (`id_kriteria`, `kode_kriteria`, `nama_kriteria`, `deskripsi`) VALUES
-(1, 'K1', 'sample', 'sample'),
-(2, 'K2', 'sample', 'sample');
+(1, 'K1', 'Linguistik-Verbal', 'Kecerdasan Linguistik (Bahasa) adalah kemampuan seseorang  untuk menggunakan bahasa dan kata-kata secara efektif, baik  secara tertulis maupun lisan, dalam berbagai bentuk yang berbeda  untuk mengekspresikan gagasan-gagasannya.'),
+(2, 'K2', 'Logika-Matematika', 'Kecerdasan Logika (Matematika) berkaitan dengan kemahiran  seseorang dalam menggunakan logika atau penalaran,  menggunakan bilangan, dan dalam berpikir kritis.'),
+(3, 'K3', 'Visual-Spacial', 'Kecerdasan Visual Spasial (Imajinasi) berkaitan dengan  kemampuan seseorang dalam memvisualisasikan gambar di  dalam benak mereka, menangkap dunia ruang visual secara tepat  atau berhubungan dengan kemampuan indera pandang dan  berimajinasi.'),
+(4, 'K4', 'Musical', 'Kecerdasan Musical (Musik) berkaitan dengan kepekaan  seseorang terhadap suara, ritme, nada, dan musik.'),
+(5, 'K5', 'Kinestetik', 'Kecerdasan Musical (Musik) berkaitan dengan kepekaan  seseorang terhadap suara, ritme, nada, dan musik.'),
+(6, 'K6', 'Interpersonal', 'Kecerdasan Interpersonal (Antara pribadi) berkaitan dengan  kemampuan seseorang dalam memahami, berinteraksi, dan  bekerja sama dengan orang lain.'),
+(7, 'K7', 'Intrapersonal', 'Kecerdasan Intrapersonal (Intropeksi) berkaitan dengan  kemampuan seseorang dalam hubungannya dengan kapasitas  introspektif, memiliki pemahaman yang mendalam tentang diri  mereka sendiri, apa kekuatan atau kelemahan dirinya, dan apa  yang membuat dirinya unik.'),
+(9, 'K8', 'Naturalisme', 'Kecerdasan Naturalis (Alami) berkaitan dengan kepekaan  seseorang dalam menghadapi fenomena alam.');
 
 -- --------------------------------------------------------
 
@@ -86,7 +119,7 @@ INSERT INTO `kriteria` (`id_kriteria`, `kode_kriteria`, `nama_kriteria`, `deskri
 
 CREATE TABLE `pertanyaan` (
   `id_pertanyaan` int(11) NOT NULL,
-  `kode_gejala` varchar(25) DEFAULT NULL,
+  `id_gejala` int(11) DEFAULT NULL,
   `kode_pertanyaan` varchar(25) DEFAULT NULL,
   `pertanyaan` tinytext DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -95,9 +128,8 @@ CREATE TABLE `pertanyaan` (
 -- Dumping data untuk tabel `pertanyaan`
 --
 
-INSERT INTO `pertanyaan` (`id_pertanyaan`, `kode_gejala`, `kode_pertanyaan`, `pertanyaan`) VALUES
-(1, 'G1', 'KP1', 'sample'),
-(2, 'G2', 'KP2', 'sample');
+INSERT INTO `pertanyaan` (`id_pertanyaan`, `id_gejala`, `kode_pertanyaan`, `pertanyaan`) VALUES
+(5, 4, 'KP-01', 'Apakah Anak Suka Bercerita ?');
 
 -- --------------------------------------------------------
 
@@ -123,7 +155,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id_user`, `username`, `password`, `nama`, `alamat`, `jenis_kelamin`, `email`, `tlp`, `level`) VALUES
 (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'cirebon', 'perempuan', 'admin@gmail.com', 238572345, 'admin'),
-(3, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'user', 'cirebon', 'perempuan', 'user@gmail.com', 2934092, 'user');
+(3, 'velya', 'ee11cbb19052e40b07aac0ca060c23ee', 'Velya', 'cirebon', 'perempuan', 'user@gmail.com', 2934092, 'user');
 
 --
 -- Indexes for dumped tables
@@ -133,15 +165,13 @@ INSERT INTO `user` (`id_user`, `username`, `password`, `nama`, `alamat`, `jenis_
 -- Indeks untuk tabel `gejala`
 --
 ALTER TABLE `gejala`
-  ADD PRIMARY KEY (`id_gejala`),
-  ADD UNIQUE KEY `kode_gejala` (`kode_gejala`);
+  ADD PRIMARY KEY (`id_gejala`);
 
 --
 -- Indeks untuk tabel `hasil`
 --
 ALTER TABLE `hasil`
-  ADD PRIMARY KEY (`id_hasil`),
-  ADD KEY `id_user` (`id_user`);
+  ADD PRIMARY KEY (`id_hasil`);
 
 --
 -- Indeks untuk tabel `kriteria`
@@ -155,8 +185,9 @@ ALTER TABLE `kriteria`
 --
 ALTER TABLE `pertanyaan`
   ADD PRIMARY KEY (`id_pertanyaan`),
-  ADD UNIQUE KEY `kode_gejala` (`kode_gejala`),
-  ADD UNIQUE KEY `kode_pertanyaan` (`kode_pertanyaan`);
+  ADD UNIQUE KEY `kode_gejala` (`id_gejala`),
+  ADD UNIQUE KEY `kode_pertanyaan` (`kode_pertanyaan`),
+  ADD UNIQUE KEY `id_gejala` (`id_gejala`);
 
 --
 -- Indeks untuk tabel `user`
@@ -172,7 +203,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `gejala`
 --
 ALTER TABLE `gejala`
-  MODIFY `id_gejala` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_gejala` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT untuk tabel `hasil`
@@ -184,29 +215,19 @@ ALTER TABLE `hasil`
 -- AUTO_INCREMENT untuk tabel `kriteria`
 --
 ALTER TABLE `kriteria`
-  MODIFY `id_kriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_kriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT untuk tabel `pertanyaan`
 --
 ALTER TABLE `pertanyaan`
-  MODIFY `id_pertanyaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_pertanyaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `hasil`
---
-ALTER TABLE `hasil`
-  ADD CONSTRAINT `hasil_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
