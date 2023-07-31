22de1a6299d9f5687f21bbdd5537d12f00dab7de
inamfalahuddin <inamfalahuddin06@gmail.com>
2023-07-31 19:55:16 +0700

    modified db

--- database/sistem_pakar.sql
+++ database/sistem_pakar.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 29 Jul 2023 pada 16.48
--- Versi server: 10.4.25-MariaDB
--- Versi PHP: 8.1.10
+-- Waktu pembuatan: 31 Jul 2023 pada 14.54
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,7 +31,7 @@ CREATE TABLE `gejala` (
   `id_gejala` int(11) NOT NULL,
   `kode_gejala` varchar(25) DEFAULT NULL,
   `nama_gejala` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `gejala`
@@ -83,7 +83,7 @@ CREATE TABLE `hasil` (
   `tanggal` date DEFAULT NULL,
   `usia` int(50) DEFAULT NULL,
   `hasil_kriteria` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -99,45 +99,47 @@ CREATE TABLE `jawaban` (
   `id_pertanyaan` int(11) DEFAULT NULL,
   `id_kriteria` int(11) DEFAULT NULL,
   `id_gejala` int(11) DEFAULT NULL,
-  `jawaban` float DEFAULT NULL,
+  `kode_gejala` varchar(25) NOT NULL,
+  `cf_user` float DEFAULT NULL,
+  `sesi` int(11) NOT NULL,
   `tanggal` date DEFAULT curdate()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `jawaban`
 --
 
-INSERT INTO `jawaban` (`id_jawaban`, `id_user`, `nama`, `usia`, `id_pertanyaan`, `id_kriteria`, `id_gejala`, `jawaban`, `tanggal`) VALUES
-(93, 3, 'Velya', 22, 5, 1, 4, 1, '2023-07-29'),
-(94, 3, 'Velya', 22, 9, 1, 5, 0.5, '2023-07-29'),
-(95, 3, 'Velya', 22, 10, 1, 6, 0.5, '2023-07-29'),
-(96, 3, 'Velya', 22, 11, 1, 7, 0.5, '2023-07-29'),
-(97, 3, 'Velya', 22, 12, 1, 8, 1, '2023-07-29'),
-(98, 3, 'Velya', 22, 13, 2, 10, 1, '2023-07-29'),
-(99, 3, 'Velya', 22, 14, 2, 11, 0, '2023-07-29'),
-(100, 3, 'Velya', 22, 15, 2, 12, 0.5, '2023-07-29'),
-(101, 3, 'Velya', 22, 16, 2, 13, 0, '2023-07-29'),
-(102, 3, 'Velya', 22, 18, 3, 14, 0, '2023-07-29'),
-(103, 3, 'Velya', 22, 19, 3, 15, 0, '2023-07-29'),
-(104, 3, 'Velya', 22, 20, 4, 16, 0.5, '2023-07-29'),
-(105, 3, 'Velya', 22, 21, 4, 17, 0.5, '2023-07-29'),
-(106, 3, 'Velya', 22, 22, 4, 20, 0.5, '2023-07-29'),
-(107, 3, 'Velya', 22, 23, 4, 21, 0.5, '2023-07-29'),
-(108, 3, 'Velya', 22, 24, 5, 22, 0.5, '2023-07-29'),
-(109, 3, 'Velya', 22, 25, 5, 23, 0, '2023-07-29'),
-(110, 3, 'Velya', 22, 26, 5, 24, 0, '2023-07-29'),
-(111, 3, 'Velya', 22, 27, 5, 25, 0, '2023-07-29'),
-(112, 3, 'Velya', 22, 28, 6, 26, 0, '2023-07-29'),
-(113, 3, 'Velya', 22, 29, 6, 27, 0, '2023-07-29'),
-(114, 3, 'Velya', 22, 30, 6, 28, 0, '2023-07-29'),
-(115, 3, 'Velya', 22, 31, 6, 29, 0.5, '2023-07-29'),
-(116, 3, 'Velya', 22, 32, 7, 30, 0.5, '2023-07-29'),
-(117, 3, 'Velya', 22, 33, 7, 31, 0.5, '2023-07-29'),
-(118, 3, 'Velya', 22, 34, 7, 32, 0, '2023-07-29'),
-(119, 3, 'Velya', 22, 35, 9, 33, 0.5, '2023-07-29'),
-(120, 3, 'Velya', 22, 36, 9, 34, 1, '2023-07-29'),
-(121, 3, 'Velya', 22, 37, 7, 35, 0.5, '2023-07-29'),
-(122, 3, 'Velya', 22, 38, 9, 36, 0.5, '2023-07-29');
+INSERT INTO `jawaban` (`id_jawaban`, `id_user`, `nama`, `usia`, `id_pertanyaan`, `id_kriteria`, `id_gejala`, `kode_gejala`, `cf_user`, `sesi`, `tanggal`) VALUES
+(93, 3, 'Velya', 22, 5, 1, 4, 'G1', 1, 1, '2023-07-29'),
+(94, 3, 'Velya', 22, 9, 1, 5, 'G2', 0.5, 1, '2023-07-29'),
+(95, 3, 'Velya', 22, 10, 1, 6, 'G3', 0.5, 1, '2023-07-29'),
+(96, 3, 'Velya', 22, 11, 1, 7, 'G4', 0.5, 1, '2023-07-29'),
+(97, 3, 'Velya', 22, 12, 1, 8, 'G5', 1, 1, '2023-07-29'),
+(98, 3, 'Velya', 22, 13, 2, 10, 'G6', 1, 1, '2023-07-29'),
+(99, 3, 'Velya', 22, 14, 2, 11, 'G7', 0, 1, '2023-07-29'),
+(100, 3, 'Velya', 22, 15, 2, 12, 'G8', 0.5, 1, '2023-07-29'),
+(101, 3, 'Velya', 22, 16, 2, 13, 'G9', 0, 1, '2023-07-29'),
+(102, 3, 'Velya', 22, 18, 3, 14, 'G10', 0, 1, '2023-07-29'),
+(103, 3, 'Velya', 22, 19, 3, 15, 'G11', 0, 1, '2023-07-29'),
+(104, 3, 'Velya', 22, 20, 4, 16, 'G12', 0.5, 1, '2023-07-29'),
+(105, 3, 'Velya', 22, 21, 4, 17, 'G13', 0.5, 1, '2023-07-29'),
+(106, 3, 'Velya', 22, 22, 4, 20, 'G14', 0.5, 1, '2023-07-29'),
+(107, 3, 'Velya', 22, 23, 4, 21, 'G15', 0.5, 1, '2023-07-29'),
+(108, 3, 'Velya', 22, 24, 5, 22, 'G16', 0.5, 1, '2023-07-29'),
+(109, 3, 'Velya', 22, 25, 5, 23, 'G17', 0, 1, '2023-07-29'),
+(110, 3, 'Velya', 22, 26, 5, 24, 'G18', 0, 1, '2023-07-29'),
+(111, 3, 'Velya', 22, 27, 5, 25, 'G19', 0, 1, '2023-07-29'),
+(112, 3, 'Velya', 22, 28, 6, 26, 'G20', 0, 1, '2023-07-29'),
+(113, 3, 'Velya', 22, 29, 6, 27, 'G21', 0, 1, '2023-07-29'),
+(114, 3, 'Velya', 22, 30, 6, 28, 'G22', 0, 1, '2023-07-29'),
+(115, 3, 'Velya', 22, 31, 6, 29, 'G23', 0.5, 1, '2023-07-29'),
+(116, 3, 'Velya', 22, 32, 7, 30, 'G24', 0.5, 1, '2023-07-29'),
+(117, 3, 'Velya', 22, 33, 7, 31, 'G25', 0.5, 1, '2023-07-29'),
+(118, 3, 'Velya', 22, 34, 7, 32, 'G26', 0, 1, '2023-07-29'),
+(119, 3, 'Velya', 22, 35, 9, 33, 'G27', 0.5, 1, '2023-07-29'),
+(120, 3, 'Velya', 22, 36, 9, 34, 'G28', 1, 1, '2023-07-29'),
+(121, 3, 'Velya', 22, 37, 7, 35, 'G29', 0.5, 1, '2023-07-29'),
+(122, 3, 'Velya', 22, 38, 9, 36, 'G30', 0.5, 1, '2023-07-29');
 
 -- --------------------------------------------------------
 
@@ -150,7 +152,7 @@ CREATE TABLE `kriteria` (
   `kode_kriteria` varchar(25) DEFAULT NULL,
   `nama_kriteria` varchar(50) DEFAULT NULL,
   `deskripsi` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `kriteria`
@@ -177,7 +179,7 @@ CREATE TABLE `pakar` (
   `kode_kriteria` varchar(10) NOT NULL,
   `kode_gejala` varchar(10) NOT NULL,
   `cf_pakar` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `pakar`
@@ -232,45 +234,46 @@ CREATE TABLE `pertanyaan` (
   `id_pertanyaan` int(11) NOT NULL,
   `id_gejala` int(11) DEFAULT NULL,
   `id_kriteria` int(11) DEFAULT NULL,
+  `kode_gejala` varchar(25) NOT NULL,
   `kode_pertanyaan` varchar(25) DEFAULT NULL,
   `pertanyaan` tinytext DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `pertanyaan`
 --
 
-INSERT INTO `pertanyaan` (`id_pertanyaan`, `id_gejala`, `id_kriteria`, `kode_pertanyaan`, `pertanyaan`) VALUES
-(5, 4, 1, 'KP-01', 'Apakah Anak Suka Bercerita ?'),
-(9, 5, 1, 'KP-02', 'Apakah Anak Suka Membaca ?'),
-(10, 6, 1, 'KP-03', 'Apakah Anak Suka Menulis ?'),
-(11, 7, 1, 'KP-04', 'Apakah Anak Suka Memecahkan Teka Teki ?'),
-(12, 8, 1, 'KP-05', 'Apakah Anak Gemar Berdebat ?'),
-(13, 10, 2, 'KP-06', 'Apakah Anak Suka Dengan Permainan Catur ?'),
-(14, 11, 2, 'KP-07', 'Apakah Anak Suka Buku Ilmu Pengetahuan ?'),
-(15, 12, 2, 'KP-08', 'Apakah Anak Suka Berhitung Tanpa Alat Bantu ?'),
-(16, 13, 2, 'KP-09', 'Apakah Anak Suka Permainan Gambar Atau 3D ?'),
-(18, 14, 3, 'KP-10', 'Apakah Anak Mudah Mengenali Pola dan Berimajinasi ?'),
-(19, 15, 3, 'KP-11', 'Apakah Anak Suka Menggambar ?'),
-(20, 16, 4, 'KP-12', 'Apakah Anak Bisa Memainkan Alat Musik ?'),
-(21, 17, 4, 'KP-13', 'Apakah Anak Suka Bernyanyi ?'),
-(22, 20, 4, 'KP-14', 'Apakah Anak Suka Mendengarkan Musik ?'),
-(23, 21, 4, 'KP-15', 'Apakah Anak Suka Belajar Di Iringi Musik ?'),
-(24, 22, 5, 'KP-16', 'Apakah Anak Suka Melakukan Kegiatan Fisik ?'),
-(25, 23, 5, 'KP-17', 'Apakah Anak Suka Menari ?'),
-(26, 24, 5, 'KP-18', 'Apakah Anak Suka Olahraga atau Bela Diri ?'),
-(27, 25, 5, 'KP-19', 'Apakah Anak Suka Menirukan Gerak Tubuh Untuk Bicara ?'),
-(28, 26, 6, 'KP-20', 'Apakah Anak Suka Bekerja Sama Dengan Orang Lain ?'),
-(29, 27, 6, 'KP-21', 'Apakah Anak Suka Berkenalan Dengan Orang Baru ? '),
-(30, 28, 6, 'KP-22', 'Apakah Anak Suka Bergaul atau Bersosialisasi ?'),
-(31, 29, 6, 'KP-23', 'Apakah Anak Memiliki Empati Yang Tinggi ?'),
-(32, 30, 7, 'KP-24', 'Apakah Anak Suka Belajar Sendiri Maupun Bermain ?'),
-(33, 31, 7, 'KP-25', 'Apakah Anak Suka Keadaan Tenang ?'),
-(34, 32, 7, 'KP-26', 'Apakah Anak Suka Hobi Yang Bersifat Pribadi ?'),
-(35, 33, 9, 'KP-27', 'Apakah Anak Suka Dengan Binatang ?'),
-(36, 34, 9, 'KP-28', 'Apakah Anak Suka Bejalan - Jalan Di Taman ?'),
-(37, 35, 7, 'KP-29', 'Apakah Anak Suka Berkebun ?'),
-(38, 36, 9, 'KP-30', 'Apakah Anak Suka Berkemah ?');
+INSERT INTO `pertanyaan` (`id_pertanyaan`, `id_gejala`, `id_kriteria`, `kode_gejala`, `kode_pertanyaan`, `pertanyaan`) VALUES
+(5, 4, 1, 'G1', 'KP-01', 'Apakah Anak Suka Bercerita ?'),
+(9, 5, 1, 'G2', 'KP-02', 'Apakah Anak Suka Membaca ?'),
+(10, 6, 1, 'G3', 'KP-03', 'Apakah Anak Suka Menulis ?'),
+(11, 7, 1, 'G4', 'KP-04', 'Apakah Anak Suka Memecahkan Teka Teki ?'),
+(12, 8, 1, 'G5', 'KP-05', 'Apakah Anak Gemar Berdebat ?'),
+(13, 10, 2, 'G6', 'KP-06', 'Apakah Anak Suka Dengan Permainan Catur ?'),
+(14, 11, 2, 'G7', 'KP-07', 'Apakah Anak Suka Buku Ilmu Pengetahuan ?'),
+(15, 12, 2, 'G8', 'KP-08', 'Apakah Anak Suka Berhitung Tanpa Alat Bantu ?'),
+(16, 13, 2, 'G9', 'KP-09', 'Apakah Anak Suka Permainan Gambar Atau 3D ?'),
+(18, 14, 3, 'G10', 'KP-10', 'Apakah Anak Mudah Mengenali Pola dan Berimajinasi ?'),
+(19, 15, 3, 'G11', 'KP-11', 'Apakah Anak Suka Menggambar ?'),
+(20, 16, 4, 'G12', 'KP-12', 'Apakah Anak Bisa Memainkan Alat Musik ?'),
+(21, 17, 4, 'G13', 'KP-13', 'Apakah Anak Suka Bernyanyi ?'),
+(22, 20, 4, 'G14', 'KP-14', 'Apakah Anak Suka Mendengarkan Musik ?'),
+(23, 21, 4, 'G15', 'KP-15', 'Apakah Anak Suka Belajar Di Iringi Musik ?'),
+(24, 22, 5, 'G16', 'KP-16', 'Apakah Anak Suka Melakukan Kegiatan Fisik ?'),
+(25, 23, 5, 'G17', 'KP-17', 'Apakah Anak Suka Menari ?'),
+(26, 24, 5, 'G18', 'KP-18', 'Apakah Anak Suka Olahraga atau Bela Diri ?'),
+(27, 25, 5, 'G19', 'KP-19', 'Apakah Anak Suka Menirukan Gerak Tubuh Untuk Bicara ?'),
+(28, 26, 6, 'G20', 'KP-20', 'Apakah Anak Suka Bekerja Sama Dengan Orang Lain ?'),
+(29, 27, 6, 'G21', 'KP-21', 'Apakah Anak Suka Berkenalan Dengan Orang Baru ? '),
+(30, 28, 6, 'G22', 'KP-22', 'Apakah Anak Suka Bergaul atau Bersosialisasi ?'),
+(31, 29, 6, 'G23', 'KP-23', 'Apakah Anak Memiliki Empati Yang Tinggi ?'),
+(32, 30, 7, 'G24', 'KP-24', 'Apakah Anak Suka Belajar Sendiri Maupun Bermain ?'),
+(33, 31, 7, 'G25', 'KP-25', 'Apakah Anak Suka Keadaan Tenang ?'),
+(34, 32, 7, 'G26', 'KP-26', 'Apakah Anak Suka Hobi Yang Bersifat Pribadi ?'),
+(35, 33, 9, 'G27', 'KP-27', 'Apakah Anak Suka Dengan Binatang ?'),
+(36, 34, 9, 'G28', 'KP-28', 'Apakah Anak Suka Bejalan - Jalan Di Taman ?'),
+(37, 35, 7, 'G29', 'KP-29', 'Apakah Anak Suka Berkebun ?'),
+(38, 36, 9, 'G30', 'KP-30', 'Apakah Anak Suka Berkemah ?');
 
 -- --------------------------------------------------------
 
@@ -288,7 +291,7 @@ CREATE TABLE `user` (
   `email` varchar(20) DEFAULT NULL,
   `tlp` int(20) DEFAULT NULL,
   `level` enum('admin','user') DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `user`
@@ -296,7 +299,64 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id_user`, `username`, `password`, `nama`, `alamat`, `jenis_kelamin`, `email`, `tlp`, `level`) VALUES
 (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'cirebon', 'perempuan', 'admin@gmail.com', 238572345, 'admin'),
-(3, 'velya', 'ee11cbb19052e40b07aac0ca060c23ee', 'Velya', 'cirebon', 'perempuan', 'user@gmail.com', 2934092, 'user');
+(3, 'velya', 'ee11cbb19052e40b07aac0ca060c23ee', 'Velya', 'cirebon', 'perempuan', 'user@gmail.com', 2934092, 'user'),
+(4, 'inamfalahuddin', '598147631c57ef841def7ae8ed9a87da', 'In\'am Falahuddin', 'kudumulya', 'laki-laki', 'inam@mail.com', 2147483647, 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `variabel`
+--
+
+CREATE TABLE `variabel` (
+  `id_variabel` int(11) NOT NULL,
+  `kode_kriteria` varchar(25) NOT NULL,
+  `kode_gejala` varchar(25) NOT NULL,
+  `cf_pakar` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `variabel`
+--
+
+INSERT INTO `variabel` (`id_variabel`, `kode_kriteria`, `kode_gejala`, `cf_pakar`) VALUES
+(1, 'K1', 'G1', 0.7),
+(2, 'K1', 'G2', 0.4),
+(3, 'K1', 'G3', 0.7),
+(4, 'K1', 'G4', 1),
+(5, 'K1', 'G5', 0.7),
+(6, 'K2', 'G2', 1),
+(7, 'K2', 'G6', 0.7),
+(8, 'K2', 'G7', 0.4),
+(9, 'K2', 'G8', 0.7),
+(10, 'K2', 'G9', 0.4),
+(11, 'K3', 'G3', 0.4),
+(12, 'K3', 'G9', 0.7),
+(13, 'K3', 'G10', 0.7),
+(14, 'K3', 'G11', 0.4),
+(15, 'K4', 'G12', 0.4),
+(16, 'K4', 'G13', 0.4),
+(17, 'K4', 'G14', 0.7),
+(18, 'K4', 'G15', 0.4),
+(19, 'K5', 'G16', 0.4),
+(20, 'K5', 'G17', 0.7),
+(21, 'K5', 'G18', 0.4),
+(22, 'K5', 'G19', 1),
+(23, 'K6', 'G5', 1),
+(24, 'K6', 'G20', 0.4),
+(25, 'K6', 'G21', 0.7),
+(26, 'K6', 'G22', 0.4),
+(27, 'K6', 'G23', 1),
+(28, 'K7', 'G2', 0.4),
+(29, 'K7', 'G7', 0.4),
+(30, 'K7', 'G15', 0.4),
+(31, 'K7', 'G24', 0.7),
+(32, 'K7', 'G25', 0.7),
+(33, 'K7', 'G26', 1),
+(34, 'K8', 'G27', 0.4),
+(35, 'K8', 'G28', 0.4),
+(36, 'K8', 'G29', 0.7),
+(37, 'K8', 'G30', 0.1);
 
 --
 -- Indexes for dumped tables
@@ -341,14 +401,19 @@ ALTER TABLE `pakar`
 -- Indeks untuk tabel `pertanyaan`
 --
 ALTER TABLE `pertanyaan`
-  ADD PRIMARY KEY (`id_pertanyaan`),
-  ADD UNIQUE KEY `id_gejala` (`id_gejala`);
+  ADD PRIMARY KEY (`id_pertanyaan`);
 
 --
 -- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
+
+--
+-- Indeks untuk tabel `variabel`
+--
+ALTER TABLE `variabel`
+  ADD PRIMARY KEY (`id_variabel`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
@@ -370,7 +435,7 @@ ALTER TABLE `hasil`
 -- AUTO_INCREMENT untuk tabel `jawaban`
 --
 ALTER TABLE `jawaban`
-  MODIFY `id_jawaban` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123;
+  MODIFY `id_jawaban` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=153;
 
 --
 -- AUTO_INCREMENT untuk tabel `kriteria`
@@ -385,16 +450,16 @@ ALTER TABLE `pakar`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
 
 --
--- AUTO_INCREMENT untuk tabel `pertanyaan`
---
-ALTER TABLE `pertanyaan`
-  MODIFY `id_pertanyaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
-
---
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `variabel`
+--
+ALTER TABLE `variabel`
+  MODIFY `id_variabel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -414,12 +479,6 @@ ALTER TABLE `jawaban`
   ADD CONSTRAINT `jawaban_ibfk_2` FOREIGN KEY (`id_pertanyaan`) REFERENCES `pertanyaan` (`id_pertanyaan`),
   ADD CONSTRAINT `jawaban_ibfk_3` FOREIGN KEY (`id_kriteria`) REFERENCES `kriteria` (`id_kriteria`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `jawaban_ibfk_4` FOREIGN KEY (`id_gejala`) REFERENCES `gejala` (`id_gejala`);
-
---
--- Ketidakleluasaan untuk tabel `pertanyaan`
---
-ALTER TABLE `pertanyaan`
-  ADD CONSTRAINT `id_gejala` FOREIGN KEY (`id_gejala`) REFERENCES `gejala` (`id_gejala`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
