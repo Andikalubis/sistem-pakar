871af4375484092760e20eaa7c7eeaa858429516
Zcrime <anandika217@gmail.com>
2023-08-31 01:39:27 +0700

    delete file (dashboard, deteksi, riwayat)

--- application/views/admin/pages/riwayat.php
+++
@@ -1,4 +0,0 @@
-<div class="d-flex flex-column justify-content-center align-items-center h-100">
-    <h1 class="text-center">Selamat Datang Untuk Mengidentifikasi Kecerdasan Minat dan Bakat Pada Anak Usia Dini</h1>
-    <button class="btn btn-primary mt-4">Mulai Deteksi</button>
-</div>
