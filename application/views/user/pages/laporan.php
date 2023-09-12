14490d1ccbaeb1ece23fc1dc0fe48026385b3334
Zcrime <anandika217@gmail.com>
2023-09-13 04:40:41 +0700

    FInish Broo

--- application/views/user/pages/laporan.php
+++ application/views/user/pages/laporan.php
@@ -1,131 +1,147 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= $title ?> - Sistem Pakar Anak </title>
+    <title><?= $title ?> - Sistem Pakar Anak</title>
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            width: 80%;
+            margin: auto;
+        }
+
+        .title {
+            text-align: center;
+            margin-top: 4rem;
+        }
+
+        h3 {
+            text-align: center;
+            margin-top: 1rem;
+        }
+
+        h5 {
+            text-align: center;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        table,
+        th,
+        td {
+            border: 1px solid black;
+        }
+
+        th,
+        td {
+            padding: 10px;
+            text-align: center;
+        }
+
+        .row {
+            margin-top: 20px;
+        }
+
+        .border-left {
+            border-left: 1px solid #000;
+            padding-left: 10px;
+        }
+
+        .italic {
+            font-style: italic;
+        }
+    </style>
 </head>
 
-<body class="hold-transition sidebar-mini layout-fixed" data-panel-auto-height-mode="height">
-    <div class="wrapper">
-        <h3 class="text-center my-4">Hasil Deteksi</h3>
+<body>
+    <div class="container">
+        <div class="title" style="text-align:center;">
+            Hasil Deteksi Riwayat Untuk Kriteria Kecerdasan Berdasarkan Perbandingan Metode Certainty Factor dan Teorema Bayes
+            <br> Sistem Pakar Anak
+            <br> Kabupaten Cirebon Jawa Barat
+        </div>
+        <hr class="line-title">
+        <h3>Hasil Deteksi</h3>
+        <p>Nama : <?= $hasil->nama ?></p>
+        <p>Usia : <?= $hasil->usia ?> tahun</p>
+        <p>Tanggal : <?= $hasil->tanggal ?></p>
+
         <div class="row">
-            <div class="col-md-12">
-                <div class="card card-default">
-                    <div class="card-header text-center">
-                        <h5 class="text-center mt-2">
-                            <?= $nama ?> (
-                            <?= $usia ?> tahun)
-                        </h5>
-                        <p> Hasil deteksi untuk kriteria kecerdasan adalah</p>
-                    </div>
-                    <div class="bg0 m-t-150 p-b-100">
-                        <div class="container">
-                            <div class="container rounded bg-white mb-2">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="p-3 py-5">
+            <div class="col-md-6">
+                <h4 class="mtext-112 cl2">Certainty Factor</h4>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Kriteria</th>
+                            <th>Bobot</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $no = 1;
+                        foreach ($hasil_cf as $data) : ?>
+                            <tr>
+                                <td><?= $no++ ?></td>
+                                <td><?= $data->kriteria_data->nama_kriteria ?></td>
+                                <td><?= $data->bobot ?>%</td>
+                            </tr>
+                        <?php endforeach ?>
+                    </tbody>
+                </table>
+            </div>
 
-                                            <div class="d-flex justify-content-between align-items-center mb-3">
-                                                <h4 class="mtext-112 cl2">Certainty Factor</h4>
-                                            </div>
-                                            <div class="card-body table-responsive p-0">
-                                                <table class="table table-hover text-nowrap">
-                                                    <thead>
-                                                        <tr>
-                                                            <th>No</th>
-                                                            <th>Kriteria</th>
-                                                            <th>Bobot</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        <?php
-                                                        $no = 1;
-                                                        foreach ($hasil_cf as $data) : ?>
-                                                            <tr>
-                                                                <td>
-                                                                    <?= $no++ ?>
-                                                                </td>
-                                                                <td>
-                                                                    <?= $data->nama ?>
-                                                                </td>
-                                                                <td>
-                                                                    <?= $data->bobot ?>%
-                                                                </td>
-                                                            </tr>
-                                                        <?php endforeach; ?>
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="col-md-6 border-left">
-                                        <div class="p-3 py-5">
-                                            <div class="d-flex justify-content-between align-items-center mb-3">
-                                                <h4 class="text-right mtext-112 cl2 ">Teorema Bayes</h4>
-                                            </div>
-
-                                            <div class="card-body table-responsive p-0">
-                                                <table class="table table-hover text-nowrap">
-                                                    <thead>
-                                                        <tr>
-                                                            <th>No</th>
-                                                            <th>Kriteria</th>
-                                                            <th>Bobot</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        <?php
-                                                        $no = 1;
-                                                        foreach ($hasil_nb as $data) : ?>
-                                                            <tr>
-                                                                <td>
-                                                                    <?= $no++ ?>
-                                                                </td>
-                                                                <td>
-                                                                    <?= $data->nama ?>
-                                                                </td>
-                                                                <td>
-                                                                    <?= $data->bobot ?>%
-                                                                </td>
-                                                            </tr>
-                                                        <?php endforeach; ?>
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="row p-4">
-                                        <div class="col">
-                                            <?php
-                                            $no = 1;
-                                            foreach ($hasil_cf as $data) : ?>
-                                                <strong class="mb-2 d-inline-block">
-                                                    <?= $data->nama ?>
-                                                </strong>
-                                                <p>
-                                                    <?= $data->deskripsi ?>
-                                                </p>
-                                                <div class="px-4 border-left border-info mb-4">
-                                                    <small style="font-style: italic;">stimulus</small>
-                                                    <p>
-                                                        <?= $data->stimulasi ?>
-                                                    </p>
-                                                </div>
-                                            <?php endforeach; ?>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- /.card -->
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="col-md-6">
+                <h4 class="mtext-112 cl2">Teorema Bayes</h4>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Kriteria</th>
+                            <th>Bobot</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $no = 1;
+                        foreach ($hasil_nb as $data) : ?>
+                            <tr>
+                                <td><?= $no++ ?></td>
+                                <td><?= $data->kriteria_data->nama_kriteria ?></td>
+                                <td><?= $data->bobot ?>%</td>
+                            </tr>
+                        <?php endforeach ?>
+                    </tbody>
+                </table>
             </div>
         </div>
+
+        <div class="row p-4 justify-content-between">
+            <div class="col-md-6">
+                <?php foreach ($hasil_cf as $data) : ?>
+                    <strong class="mb-2 d-inline-block">
+                        <?= $data->kriteria_data->nama_kriteria ?>
+                    </strong>
+                    <p><?= $data->kriteria_data->deskripsi ?></p>
+                    <div class="px-4 border-left border-info mb-4">
+                        <small style="font-style: italic;">stimulus</small>
+                        <p><?= $data->kriteria_data->stimulasi ?></p>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
